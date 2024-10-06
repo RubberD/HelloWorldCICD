151265d7ff6eb78171e3af6eefa6c1801e68c81a
Lasse Storgaard <lasse.storgaard@gmail.com>
2024-10-06 10:53:55 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const { createServer } = require('node:http');
-const hostname = '127.0.0.1';
-const port = 3000;
+const hostname = '46.101.250.218';
+const port = 80;
 const server = createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
